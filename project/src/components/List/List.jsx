2cb3797b80cdf7d79aca5460916ab23875155a83
hannasghansson <hannagrapenros@hotmail.se>
2023-02-06 10:31:49 +0100

    add more styling

--- project/src/components/List/List.jsx
+++ project/src/components/List/List.jsx
@@ -18,61 +18,38 @@ const List = () => {
     <div class="container">
       <div class="row d-flex justify-content-center mt-5 ">
         <div class="col-md-12">
-          <div>
-            {/* <div class="card"> */}
-            {/* <div class="d-flex justify-content-between align-items-center">
-              <span class="font-weight-bold">Weekly Tasks</span>
-
-              <div class="d-flex flex-row">
-                <button class="btn btn-primary mr-2 active">Active</button>
-                <button class="btn btn-primary new">
-                  <i class="fa fa-plus"></i> New
-                </button>
-              </div>
-            </div> */}
-
-            {/* <div class="mt-3 inputs">
-              <i class="fa fa-search"></i>
-              <input
-                type="text"
-                class="form-control "
-                placeholder="Search Tasks..."
-              />
-            </div> */}
-
-            <div class="border-bottom my-1">
-              <div class="d-flex justify-content-between align-items-center ">
-                <div class="d-flex flex-row align-items-center ">
-                  <img
-                    className="me-3"
-                    style={{
-                      width: 50,
-                      height: 50,
-                      objectFit: "cover",
-                      objectPosition: "center",
-                      borderRadius: 50,
-                    }}
-                    src="https://img.freepik.com/free-vector/cute-baby-lion-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3783.jpg?w=1060&t=st=1675430518~exp=1675431118~hmac=8a2a842d984a92808a19e34fffa63d666b3e43785fac01e7ca1fd2c6ebcddfbd"
-                    alt="teacher"
-                  />
-                  <div class="d-flex flex-column">
-                    <h5 className="m-0">Namn Efternamn</h5>
-                    <div class="d-flex flex-row align-items-center time-text">
-                      <p className="m-0">Lorem Ipsum</p>
-                    </div>
+          <div class="border-bottom my-1">
+            <div class="d-flex justify-content-between align-items-center ">
+              <div class="d-flex flex-row align-items-center ">
+                <img
+                  className="me-3"
+                  style={{
+                    width: 50,
+                    height: 50,
+                    objectFit: "cover",
+                    objectPosition: "center",
+                    borderRadius: 50,
+                  }}
+                  src="https://img.freepik.com/free-vector/cute-baby-lion-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3783.jpg?w=1060&t=st=1675430518~exp=1675431118~hmac=8a2a842d984a92808a19e34fffa63d666b3e43785fac01e7ca1fd2c6ebcddfbd"
+                  alt="teacher"
+                />
+                <div class="d-flex flex-column">
+                  <h5 className="m-0">Namn Efternamn</h5>
+                  <div class="d-flex flex-row align-items-center time-text">
+                    <p className="m-0">Lorem Ipsum</p>
                   </div>
                 </div>
-                <div
-                  onClick={() => {
-                    setReadMore(!readMore);
-                  }}
-                >
-                  <span>{linkName}</span>
-                </div>
               </div>
-
-              <p className="my-3">{readMore && content}</p>
+              <div
+                onClick={() => {
+                  setReadMore(!readMore);
+                }}
+              >
+                <span>{linkName}</span>
+              </div>
             </div>
+
+            <p className="my-3">{readMore && content}</p>
           </div>
         </div>
       </div>
