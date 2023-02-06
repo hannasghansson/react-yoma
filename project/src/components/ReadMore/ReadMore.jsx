88340d747fc70a6abe225a9d484128da89047468
hannasghansson <hannagrapenros@hotmail.se>
2023-02-06 10:02:33 +0100

    add styling

--- project/src/components/ReadMore/ReadMore.jsx
+++ project/src/components/ReadMore/ReadMore.jsx
@@ -15,7 +15,7 @@ const ReadMore = () => {
   );
   const linkName = readMore ? <IoIosArrowUp /> : <IoIosArrowDown />;
   return (
-    <div className="container">
+    <div className="container py-4">
       <div className="border-bottom">
         <div
           onClick={() => {
@@ -23,8 +23,25 @@ const ReadMore = () => {
           }}
         >
           <div className="row">
-            <h2 className="col">Namn Efternamn</h2>
-            <span className="col">{linkName}</span>
+            <div className="col-auto">
+              <img
+                className="img-fluid"
+                style={{
+                  width: 35,
+                  height: 35,
+                  objectFit: "cover",
+                  objectPosition: "center",
+                  borderRadius: 32,
+                }}
+                src="https://img.freepik.com/free-vector/cute-baby-lion-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3783.jpg?w=1060&t=st=1675430518~exp=1675431118~hmac=8a2a842d984a92808a19e34fffa63d666b3e43785fac01e7ca1fd2c6ebcddfbd"
+                alt="teacher"
+              />
+            </div>
+
+            <h6 className="col d-flex justify-content-start align-items-start">
+              Namn Efternamn
+            </h6>
+            <span className="col-auto">{linkName}</span>
           </div>
 
           <p>{readMore && content}</p>
