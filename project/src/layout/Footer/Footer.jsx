ab561173116fa7a91196d8d031755756b5ba23d4
hannasghansson <hannagrapenros@hotmail.se>
2023-02-09 14:56:31 +0100

    add footer and nav

---
+++ project/src/layout/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-5" style={{ background: "#D2B569" }}>
+      <div className="container ">
+        <img
+          width={200}
+          src="https://yomacenter.se/wp-content/themes/bz-app-theme/assets/images/YOMA_Eng.svg"
+          alt=""
+        />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- project/src/layout/Navbar/Navbar.jsx
+++ project/src/layout/Navbar/Navbar.jsx
@@ -5,18 +5,18 @@ const Navbar = () => {
     <header>
       <nav className="container d-flex justify-content-between align-items-center py-4">
         <div className="d-flex align-items-baseline">
-          <h6>CompanyName</h6>
-          {/* <img
-            width={50}
-            src=""
+          <img
+            width={150}
+            src="https://yomacenter.se/wp-content/themes/bz-app-theme/assets/images/YOMA_Eng.svg"
             alt=""
-          /> */}
+          />
         </div>
 
         <ul className="list-unstyled d-flex gap-4 align-items-baseline mb-0">
-          <li> Link 1</li>
-          <li> Link 2</li>
-          <li> Link 3</li>
+          <li>KURSER</li>
+          <li>KONSERTER & INSPELNINGAR</li>
+          <li>POLSTJÄRNEPRISET</li>
+          <li>OM NATIONELLT CENTRUM</li>
           <button className="btn btn-sm btn-outline-light ">Login</button>
         </ul>
       </nav>
